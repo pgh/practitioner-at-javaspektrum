065ef13c55782dbe6f52e1c38a52ac5f49831fa9
Phillip Ghadir <ghadir@Phillip-Ghadirs-MacBook-Pro.local>
2011-05-18 23:38:12 +0200

    PG: javadoc added

---
+++ src/de/ghadir/practitioner/js_2011_04/scannerBasedOnEnums/package-info.java
@@ -0,0 +1,30 @@
+/*
+ *   Copyright 2011 Phillip Ghadir
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied. See the License for the specific
+ *   language governing permissions and limitations under the License.
+ */
+
+/**
+ * This package is used to demonstrate an approach to
+ * implement a simple state machine in a distributed way by encapsulating
+ * the implementation of every state transition in a separate object.
+ *
+ * <br/>
+ *
+ * Please recognize the choice of visibility modifiers. The visibility of
+ * the package contents is designed to provide a very simple access to
+ * the features of the package.
+ *
+ * @author Phillip Ghadir, phillip.ghadir@innoq.com
+ * */
+package de.ghadir.practitioner.js_2011_04.scannerBasedOnEnums;
