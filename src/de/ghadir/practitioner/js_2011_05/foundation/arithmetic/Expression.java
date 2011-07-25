e6c3a1617804484cad47ddb24f81a111c5887715
Phillip Ghadir <ghadir@Phillip-Ghadirs-MacBook-Pro.local>
2011-07-25 09:20:42 +0200

    PG: added dummy api for arithmetic-binding

---
+++ src/de/ghadir/practitioner/js_2011_05/foundation/arithmetic/Expression.java
@@ -0,0 +1,27 @@
+/*
+ *   Copyright 2011 Phillip Ghadir
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *   either express or implied. See the License for the specific
+ *   language governing permissions and limitations under the License.
+ */
+
+package de.ghadir.practitioner.js_2011_05.foundation.arithmetic;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Phillip Ghadir, phillip.ghadir@innoq.com
+ * @since 7/25/11 9:16 AM
+ */
+public abstract class Expression {
+    public abstract BigDecimal evalInCtx(Binding env);
+}
